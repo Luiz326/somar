370773f782a2f025cf6e1de05315d39fbb5af912
Luiz Mitsuru Dai <61781452+Luiz326@users.noreply.github.com>
2021-12-07 20:09:52 -0300

    .env adicionado

--- src/App.js
+++ src/App.js
@@ -1,13 +1,21 @@
 import { Component } from "react";
 import Soma from "./components/Somar";
+import env from "react-dotenv";
 
+
+export function MyComponent() {
+  return <div>{env.API_URL}</div>;
+}
 
 class App extends Component {
   
   render() {
     return (
       <div >
+
+        <h1>{process.env.REACT_APP_TITLE}</h1>      
         <Soma/>
+        
       </div>
 
     )
